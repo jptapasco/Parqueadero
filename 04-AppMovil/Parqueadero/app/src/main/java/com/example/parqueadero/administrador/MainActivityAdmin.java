842d4567774082327c4dac8ddc32e08d8a41eb7f
jptapasco <restrepofreddy28@gmail.com>
2024-02-17 18:25:51 -0500

    Listo para consumir API

---
+++ 04-AppMovil/Parqueadero/app/src/main/java/com/example/parqueadero/administrador/MainActivityAdmin.java
@@ -0,0 +1,16 @@
+package com.example.parqueadero.administrador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.parqueadero.R;
+
+public class MainActivityAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_admin);
+    }
+}

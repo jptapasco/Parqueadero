95a1e6af0799b34b61a94fe3083776f9a99cdf38
jptapasco <restrepofreddy28@gmail.com>
2024-02-20 17:51:09 -0500

    Adaptador Persona

--- 04-AppMovil/Parqueadero/app/src/main/java/com/example/parqueadero/administrador/Vendedores.java
+++ 04-AppMovil/Parqueadero/app/src/main/java/com/example/parqueadero/administrador/Vendedores.java
@@ -2,6 +2,9 @@ package com.example.parqueadero.administrador;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,14 +12,37 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.parqueadero.MainActivity;
 import com.example.parqueadero.R;
+import com.example.parqueadero.utils.Config;
+import com.example.parqueadero.utils.Parqueadero;
+import com.example.parqueadero.utils.ParqueaderoAdapter;
+import com.example.parqueadero.utils.Persona;
+import com.example.parqueadero.utils.PersonaAdapter;
 import com.example.parqueadero.vendedor.MainActivityVendedor;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Vendedores extends AppCompatActivity {
     Button btnParqueadero;
     Button btnVendedor;
     Button btnSalir;
+    Config dataConfig;
+    List<Persona> listaPersona;
+    RecyclerView recyclerView;
+    PersonaAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +69,56 @@ public class Vendedores extends AppCompatActivity {
                 startActivity(intencion);
             }
         });
+
+        dataConfig = new Config(getApplicationContext());
+        recyclerView = findViewById(R.id.recyclerListaVendedores);
+        listaPersona = new ArrayList<>();
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        vendedoresConAsignacion();
+    }
+
+    public void vendedoresConAsignacion(){
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = dataConfig.getEndPoint("/API-Personas/ObtenerPersonas.php");
+        StringRequest solicitud = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject respuesta = new JSONObject(response);
+                    System.out.println("Respuesta API Personas Con Asignacion: "+respuesta);
+                    cargarListaPersonasAsignadas(respuesta.getJSONArray("registros"));
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("JOA Mani el servidor GET responde con un error");
+                System.out.println(error.getMessage());
+            }
+        });
+        queue.add(solicitud);
+    }
+
+    public void cargarListaPersonasAsignadas(JSONArray datos){
+        listaPersona = new ArrayList<>();
+        for (int i = 0 ; i < datos.length() ; i++ ){
+            try {
+                JSONObject vendedor = datos.getJSONObject(i);
+                listaPersona.add(new Persona(vendedor.getString("cedula"),vendedor.getString("nombre"), vendedor.getString("apellidos")));
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        adapter = new PersonaAdapter(listaPersona);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
+    }
+
+
 }
---
+++ 04-AppMovil/Parqueadero/app/src/main/java/com/example/parqueadero/utils/Persona.java
@@ -0,0 +1,64 @@
+package com.example.parqueadero.utils;
+
+public class Persona {
+    String documento;
+    String nombre;
+    String apellido;
+    String telefono;
+    String email;
+    String password;
+
+    public Persona(String documento, String nombre, String apellido) {
+        this.documento = documento;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

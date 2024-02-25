59783b2bbef675dafdb180943590d74854318be0
jptapasco <restrepofreddy28@gmail.com>
2024-02-25 11:12:32 -0500

    Lista de Tarifas

--- 04-AppMovil/Parqueadero/app/src/main/java/com/example/parqueadero/vendedor/Tarifas.java
+++ 04-AppMovil/Parqueadero/app/src/main/java/com/example/parqueadero/vendedor/Tarifas.java
@@ -1,21 +1,49 @@
 package com.example.parqueadero.vendedor;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.parqueadero.MainActivity;
 import com.example.parqueadero.R;
+import com.example.parqueadero.utils.Config;
+import com.example.parqueadero.utils.DetalleHistorial;
+import com.example.parqueadero.utils.DetalleHistorialAdapter;
+import com.example.parqueadero.utils.Tarifa;
+import com.example.parqueadero.utils.TarifaAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tarifas extends AppCompatActivity {
 
+    Config dataConfig;
+    List<Tarifa> listaTarifa;
+    RecyclerView recyclerView;
+    TarifaAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tarifas);
+
+        dataConfig = new Config(getApplicationContext());
+        recyclerView = findViewById(R.id.recyclerTarifa);
 
         ImageView btnParqueaderoV = findViewById(R.id.btnParqueaderoV);
         ImageView btnEntrada = findViewById(R.id.btnEntradaV);
@@ -53,5 +81,51 @@ public class Tarifas extends AppCompatActivity {
         });
 
         btnTarifa.setEnabled(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        apiObtenerTarifa();
+    }
+
+    public void apiObtenerTarifa(){
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = dataConfig.getEndPoint("/API-tarifas/Obtener.php");
+        StringRequest solicitud = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject respuesta = new JSONObject(response);
+                    cargarListaTarifa(respuesta.getJSONArray("registros"));
+                } catch (JSONException e) {
+                    System.out.println("El servidor GET responde con error");
+                    System.out.println(e.getMessage());
+                    Toast.makeText(getApplicationContext(), "Error en datos del servidor: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("JOA Mani el servidor GET responde con un error");
+                System.out.println(error.getMessage());
+            }
+        });
+        queue.add(solicitud);
+    }
+
+    public void cargarListaTarifa(JSONArray datos) {
+        listaTarifa = new ArrayList<>();
+        for (int i = 0; i < datos.length(); i++) {
+            try {
+                JSONObject tarifas = datos.getJSONObject(i);
+                listaTarifa.add(new Tarifa(tarifas.getString("id"),tarifas.getString("tipo_vehiculo"),tarifas.getString("Tarifa")));
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        adapter = new TarifaAdapter(listaTarifa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.setAdapter(adapter);
     }
 }

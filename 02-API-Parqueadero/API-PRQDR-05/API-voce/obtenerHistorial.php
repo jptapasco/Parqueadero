dc3c650ca5abfc6a1c020eb2188f4cce75c9fc93
jptapasco <restrepofreddy28@gmail.com>
2024-02-27 20:34:06 -0500

    db y Apis Update

--- 02-API-Parqueadero/API-PRQDR-05/API-voce/obtenerHistorial.php
+++ 02-API-Parqueadero/API-PRQDR-05/API-voce/obtenerHistorial.php
@@ -5,13 +5,28 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 include '../Conexion.php';
 
-    //$consulta = $base_de_datos->query("SELECT * FROM ticket"); 
-$consulta = $base_de_datos->query("SELECT t.id, t.placa, t.create_entrada, t.salida, tar.tipo_vehiculo, tar.Tarifa, rv.responsable
-    FROM ticket t
-    LEFT JOIN tarifas tar ON t.id_tarifa = tar.id
-    LEFT JOIN registro_vehiculos rv ON t.placa = rv.placa WHERE t.salida IS NOT NULL");
-$datos = $consulta->fetchAll();
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id_asignacion'])) {
+    $id_asignacion = $_POST['id_asignacion'];
 
-$respuesta['registros'] = $datos;
+    $consulta = $base_de_datos->prepare("SELECT t.id, t.placa, t.create_entrada, t.salida, tar.tipo_vehiculo, tar.Tarifa, rv.responsable
+        FROM ticket t
+        LEFT JOIN tarifas tar ON t.id_tarifa = tar.id
+        LEFT JOIN registro_vehiculos rv ON t.placa = rv.placa WHERE t.salida IS NOT NULL AND id_asignacion = :ida");
+    $consulta->bindParam(':ida', $id_asignacion);
+    
+    // Ejecutar la consulta
+    $consulta->execute();
+    
+    // Obtener los datos
+    $datos = $consulta->fetchAll();
+    
+    $respuesta['registros'] = $datos;
+} else {
+    $respuesta = [
+        'status' => false,
+        'message' => "Datos incompletos"
+    ];
+}
+
 echo json_encode($respuesta);
 ?>
--- 02-API-Parqueadero/API-PRQDR-05/API-voce/obtenerParqueadero.php
+++ 02-API-Parqueadero/API-PRQDR-05/API-voce/obtenerParqueadero.php
@@ -6,24 +6,34 @@ header("Access-Control-Allow-Headers: Content-Type");
 
 include '../Conexion.php';
 
-if (!$base_de_datos) {
-    echo json_encode(['error' => 'No se pudo conectar a la base de datos.']);
-    exit();
-}
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['id_asignacion'])) {
 
-$consulta = $base_de_datos->query(" SELECT t.id, tar.tipo_vehiculo, rv.placa, rv.responsable, tar.Tarifa, t.create_entrada FROM ticket t
+    $id_asignacion = $_POST['id_asignacion'];
+
+    $consulta = $base_de_datos->prepare("SELECT t.id, tar.tipo_vehiculo, rv.placa, rv.responsable, tar.Tarifa, t.create_entrada FROM ticket t
     JOIN tarifas tar ON t.id_tarifa = tar.id
-    JOIN registro_vehiculos rv ON t.placa = rv.placa WHERE t.salida IS NULL
-");
+    JOIN registro_vehiculos rv ON t.placa = rv.placa WHERE t.salida IS NULL AND id_asignacion = :ida");
+    $consulta->bindParam(':ida', $id_asignacion);
 
-// Verifica si la consulta se realizó correctamente
-if (!$consulta) {
-    echo json_encode(['error' => 'Error en la consulta: ' . implode(":", $base_de_datos->errorInfo())]);
-    exit();
+    // Ejecutar la consulta
+    $consulta->execute();
+
+    // Verifica si la consulta se realizó correctamente
+    if (!$consulta) {
+        echo json_encode(['error' => 'Error en la consulta: ' . implode(":", $base_de_datos->errorInfo())]);
+        exit();
+    }
+
+    $datos = $consulta->fetchAll();
+
+    $respuesta['registros'] = $datos;
+
+} else {
+    $respuesta = [
+        'status' => false,
+        'message' => "Datos incompletos"
+    ];
 }
 
-$datos = $consulta->fetchAll();
-
-$respuesta['registros'] = $datos;
 echo json_encode($respuesta);
 ?>
